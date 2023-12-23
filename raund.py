c9bd1b5239c19cf5333bdde8fc30f78fc0b42383
DANIIL STEKOLSHCHIKOV <143276461+DS-975@users.noreply.github.com>
2023-12-23 19:04:52 +0400

    glava_1

---
+++ raund.py
@@ -0,0 +1,8 @@
+namber = 12.7
+print(round(namber))
+namberPropis = float(input('Введите число с плавающей точкой: '))
+print(f'Вы ввели: ',namberPropis,
+      '\n Округлим его до:', round(namberPropis))
+
+
+
